7373e64c17a0007bf39ae70aaf18741bc1c0e230
Bradley <bpoulett@ualberta.ca>
2014-12-04 12:35:33 -0800

    FIXED: LoadLibrary Failed bug.
    Added inpout32.dll to DLLs from setup.py :)

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup, Extension
+
+data_files = [("DLLs", ["inpout32.dll"])]
+
 setup(name='PortFunctions', version='1.0',
       description = 'A Windows port reader for Python, built in C',
       author = 'Bradley Poulette',
       author_email = 'bpoulett@ualberta.ca',
+      data_files = data_files,
       ext_modules=[Extension('readPort', ['PortFunctions.c'])])
